575aa10b74becec05374fb5aa0763ef1fd6bffd8
Mateus Facioli <mateus>
2022-07-02 15:00:41 -0300

    Initial Commit

---
+++ Stanford/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stanford
+//
+//  Created by Mateus Rodrigues on 02/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stanford/StanfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordApp.swift
+//  Stanford
+//
+//  Created by Mateus Rodrigues on 02/07/22.
+//
+
+import SwiftUI
+
+@main
+struct StanfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
